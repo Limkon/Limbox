68c2786f1b866b60b50f88d1dc3964fe9db2a814
Limkon <limkon@skvip.eu.org>
2025-12-13 18:19:05 +0800

    Update config.h

--- config.h
+++ config.h
@@ -4,31 +4,8 @@
 #include "common.h"
 #include "cJSON.h"
 
-// 全局配置变量
-extern int g_localPort;
-extern int g_hotkeyModifiers;
-extern int g_hotkeyVk;
-extern int g_hideTrayStart;
-
-// 抗封锁策略配置
-extern int g_enableChromeCiphers;
-extern int g_enableALPN;
-extern int g_enableFragment;
-extern int g_fragSizeMin;
-extern int g_fragSizeMax;
-extern int g_fragDelayMs;
-extern int g_enablePadding;
-extern int g_padSizeMin;
-extern int g_padSizeMax;
-extern int g_uaPlatformIndex;
-extern char g_userAgentStr[512];
-
-// 节点管理相关
-extern wchar_t g_iniFilePath[MAX_PATH];
-extern struct ProxyConfig g_proxyConfig;
-extern wchar_t** nodeTags;
-extern int nodeCount;
-extern wchar_t currentNode[64];
+// 注意：原有的全局变量 (g_localPort, g_proxyConfig 等) 已在 common.h 中声明
+// 我们直接使用它们，不再重复声明
 
 // --- 新增：订阅管理系统 ---
 #define MAX_SUBS 20
@@ -62,7 +39,6 @@ int ImportFromClipboard();
 void ToggleTrayIcon();
 
 // --- 新增：更新所有订阅 ---
-// forceMsg: 是否强制弹窗/记录日志（自动更新时设为FALSE以避免打扰）
 int UpdateAllSubscriptions(BOOL forceMsg);
 
 #endif // CONFIG_H
